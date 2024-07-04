c5305d4635d54ca08a6dd79d6debe9b774b7fac2
hasandg <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>
2024-07-04 23:44:34 +0300

    Enable Security for Eureka Discovery Server

--- PhotoAppDiscoveryService/src/main/java/com/appsdeveloperblog/photoapp/discovery/WebSecurity.java
+++ PhotoAppDiscoveryService/src/main/java/com/appsdeveloperblog/photoapp/discovery/WebSecurity.java
@@ -1,32 +1,23 @@
-/*
 package com.appsdeveloperblog.photoapp.discovery;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
 @EnableWebSecurity
 public class WebSecurity {
 
+	@Bean
+	public SecurityFilterChain configure(HttpSecurity http) throws Exception {
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/token/**").permitAll()
-                        .anyRequest().authenticated()
-                )
-                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .oauth2ResourceServer((oauth2) -> oauth2
-                        .jwt(Customizer.withDefaults())
-                ).httpBasic(Customizer.withDefaults());
-        return http.build();
-    }
+		http.csrf((csrf) -> csrf.disable());
+		http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated())
+				.httpBasic(Customizer.withDefaults());
 
+		return http.build();
+	}
 }
-*/
